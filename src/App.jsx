e3f20c625f4382808017b55c65bf908607c367d7
cvmgo <design@newlightarts.com>
2025-01-05 21:37:50 -0500

    project setups

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+		<div className='App'>
+			<header className='App-header'>
+				<img
+					src='/inside-philanthropy-logo-square.png'
+					alt='Logo'
+					className='w-16 h-auto'
+				/>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
